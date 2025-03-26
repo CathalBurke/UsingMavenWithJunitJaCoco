c76bfc2b8efe396dd5236efade89a828fd604a19
Cathal <g00413483@atu.ie>
2025-03-26 20:45:55 +0000

    added passengertests class setup. designed tests

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,48 @@
+import ie.atu.Passenger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class PassengerTest {
+
+    Passenger passenger;
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+    @Test
+    void testValidPassenger() {
+        passenger = new Passenger("Ms", "Sarah", "Jones");
+        assertEquals("Ms", passenger.getTitle());
+        assertEquals("Sarah", passenger.getFirstName());
+        assertEquals("Jones", passenger.getLastName());
+    }
+
+    @Test
+    void testInvalidTitle() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
+            new Passenger("Dr", "John", "Smith");
+        });
+        assertEquals("Invalid title. Title must be 'Mr', 'Mrs', or 'Ms'", ex.getMessage());
+    }
+
+    @Test
+    void testShortFirstName() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
+            new Passenger("Mr", "Jo", "Smith");
+        });
+        assertEquals("First name must be at least 3 characters long", ex.getMessage());
+    }
+
+    @Test
+    void testShortLastName() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> {
+            new Passenger("Mrs", "Alice", "Li");
+        });
+        assertEquals("Last name must be at least 3 characters long", ex.getMessage());
+    }
 }
