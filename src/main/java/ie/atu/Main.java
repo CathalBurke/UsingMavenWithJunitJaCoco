79813cb67c4e3f3858e7da0d1c7ace2697faa1b6
Cathal <g00413483@atu.ie>
2025-03-26 20:51:24 +0000

    added main class that allows the user to enter a passenger into the program using the keyboard to test the functionality and throws response if entered invalid data

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,17 +1,33 @@
 package ie.atu;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter passenger details:");
+        System.out.print("Title (Mr, Mrs, Ms): ");
+        String title = scanner.nextLine();
+
+        System.out.print("First name (min 3 chars): ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Last name (min 3 chars): ");
+        String lastName = scanner.nextLine();
+
+        try {
+            Passenger passenger = new Passenger(title, firstName, lastName);
+            System.out.println("\nPASSENGER CREATED SUCCESSFULLY:");
+            System.out.println(passenger.getTitle() + " " +
+                    passenger.getFirstName() + " " +
+                    passenger.getLastName());
+        } catch (IllegalArgumentException ex) {
+            System.out.println("\nERROR: " + ex.getMessage());
         }
+
+        scanner.close();
     }
 }
